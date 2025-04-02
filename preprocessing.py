bd1a1377bf3d30f63d7fbb974e0f33064f193cd7
Samuel Sahel-Schackis <65373877+samsahsch@users.noreply.github.com>
2025-04-02 16:12:27 -0700

    computing required variables

--- preprocessing.py
+++ preprocessing.py
@@ -76,3 +76,45 @@ print('Opening RUN' + str(RUNID) + '...')
 start_time = time.time()
 
 run = open_run(proposal=3408, run=RUNID)
+
+
+
+### Changing Variables
+
+
+### Function by EuXFEL staff to compute number of pulses per train, and some number 16?
+
+def get_stacking(run):
+    bunch_table = run["SQS_RR_UTC/TSYS/TIMESERVER:outputBunchPattern",'data.bunchPatternTable'].ndarray()
+    
+    DESTINATION_MASK = 0xf << 18
+    DESTINATION_T4D = 4 << 18   # SASE1/3 dump
+    PHOTON_LINE_DEFLECTION = 1 << 27  # Soft kick (e.g. SA3)
+
+    matched = (bunch_table & DESTINATION_MASK) == DESTINATION_T4D 
+    matched &= (bunch_table & PHOTON_LINE_DEFLECTION) != 0
+
+    SASE3_pattern = np.ones_like(matched[0]) &  matched[1]
+    test = (  SASE3_pattern ) *1e8
+    return np.sum( SASE3_pattern) , np.mean(np.diff(np.nonzero(SASE3_pattern)))
+
+
+
+PULSES_PER_TRAIN, EVERYNTH = get_stacking(run)
+N_CHANNELS_PULSE = int(EVERYNTH/2)*1760 # bunch interval? times 1760? gives number of channels per pulse
+filename = 'datarun' + str(RUNID) + '.h5'
+
+
+# Data where all trains are present
+gasattenuator_transmission = run['SA3_XTD10_VAC/MDL/GATT_TRANSMISSION_MONITOR', 'Estimated_Tr.value'].xarray()
+
+
+first_trainid = int(gasattenuator_transmission.trainId[0])
+trains = gasattenuator_transmission.shape[0]
+
+
+if os.path.exists(filename):
+    os.remove(filename)
+    print(f"The previous file '{filename}' has been deleted.")
+else:
+    print(f"The file '{filename}' will be created.")
